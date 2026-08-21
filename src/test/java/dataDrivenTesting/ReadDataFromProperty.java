4b3373b4af7813403aebff7814ea55803273ab21
roypriyasa <roypriyasa730@gmail.com>
2026-08-22 00:15:08 +0530

    all push

--- src/test/java/dataDrivenTesting/ReadDataFromProperty.java
+++ src/test/java/dataDrivenTesting/ReadDataFromProperty.java
@@ -14,22 +14,13 @@ public class ReadDataFromProperty {
 	public static void main(String[] args) throws IOException, InterruptedException {
 		WebDriver driver=new ChromeDriver();
 		
-		FileInputStream fis = new FileInputStream("./src/test/resources/Testdata/commondata.propertise.rtf");
+		FileInputStream fis = new FileInputStream("./src/test/resources/Testdata/config.properties");
 		        Properties p = new Properties();
 
 		        p.load(fis);
 
 		        String data = p.getProperty("url");
-		        
-		        driver.get(data);
-
-		        String un = p.getProperty("username");
-		        driver.findElement(By.id("username")).sendKeys(un);
-
-		        String pwd = p.getProperty("password");
-		        driver.findElement(By.name("pwd")).sendKeys(pwd);
-
-		        driver.findElement(By.xpath("//div[text()='Login']")).click();
+		        System.out.println(p.getProperty("browser"));
 
 		        Thread.sleep(6000);
 
