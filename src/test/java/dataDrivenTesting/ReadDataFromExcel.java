366e8f7c5669b809a2e3a51886e1005e53b4d077
roypriyasa <roypriyasa730@gmail.com>
2026-08-26 01:27:27 +0530

    readfromexcel

--- src/test/java/dataDrivenTesting/ReadDataFromExcel.java
+++ src/test/java/dataDrivenTesting/ReadDataFromExcel.java
@@ -1,10 +1,36 @@
 package dataDrivenTesting;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+
+
 public class ReadDataFromExcel {
 
-	public static void main(String[] args) {
-		 new FileinputStream();
+	public static void main(String[] args) throws IOException {
+		FileInputStream fis = new FileInputStream("./src/test/resources/Testsdata/customerapitestingdata.xlsx");
+		 Workbook workbook = new XSSFWorkbook(fis);
 
+	        Sheet sheet = workbook.getSheet("Sheet1");
+
+	        for (Row row : sheet) {
+
+	            for (Cell cell : row) {
+
+	                System.out.print(cell.toString() + "\t");
+	            }
+
+	            System.out.println();
+	        }
+
+	        workbook.close();
+	        fis.close();
+	    
 	}
 
 }
