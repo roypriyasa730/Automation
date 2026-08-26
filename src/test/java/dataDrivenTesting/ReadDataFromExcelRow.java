87b2c26cb96a452829da59263d6ff157c0c20d93
roypriyasa <roypriyasa730@gmail.com>
2026-08-27 00:19:03 +0530

    puch by ReadDataFromExc

---
+++ src/test/java/dataDrivenTesting/ReadDataFromExcelRow.java
@@ -0,0 +1,36 @@
+package dataDrivenTesting;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+public class ReadDataFromExcelRow {
+	 public static void main(String[] args) throws IOException {
+
+	        FileInputStream fis = new FileInputStream(
+	                "./src/test/resources/Testsdata/customerapitestingdata.xlsx");
+
+	        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+
+	        Sheet sheet = workbook.getSheet("Sheet1");
+
+	        // Read row 1, column 0
+	        String username = sheet.getRow(1).getCell(0).getStringCellValue();
+
+	        // Read row 1, column 1
+	        String password = sheet.getRow(1).getCell(1).getStringCellValue();
+
+	        // Read row 1, column 2
+	        String email = sheet.getRow(1).getCell(2).getStringCellValue();
+
+	        System.out.println(username);
+	        System.out.println(password);
+	        System.out.println(email);
+
+	        workbook.close();
+	        fis.close();
+	    }
+
+}
