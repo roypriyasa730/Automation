764534cc35de21fc0a1c1839afe593c86f58f35c
roypriyasa <roypriyasa730@gmail.com>
2026-08-29 11:54:15 +0530

    ok

--- src/test/java/dataDrivenTesting/ReadDataFromExcelRow.java
+++ src/test/java/dataDrivenTesting/ReadDataFromExcelRow.java
@@ -1,11 +1,8 @@
 package dataDrivenTesting;
 import java.io.FileInputStream;
 import java.io.IOException;
-
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
-
 public class ReadDataFromExcelRow {
 	 public static void main(String[] args) throws IOException {
 
