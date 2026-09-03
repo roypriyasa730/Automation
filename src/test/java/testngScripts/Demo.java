b14e13e07bcdec8926c4285bc037c52f07957dca
roypriyasa <roypriyasa730@gmail.com>
2026-09-03 12:58:59 +0530

    Reporter log

--- src/test/java/testngScripts/Demo.java
+++ src/test/java/testngScripts/Demo.java
@@ -6,6 +6,18 @@ import org.testng.annotations.Test;
 public class Demo {
 @Test
 public void Test1() {
-	Reporter.log("hello",true);
+
+	Reporter.log("Test1 execute",true);
+
+	
+}
+
+@Test
+public void Test2() {
+	Reporter.log("Test2 execute",true);
+}
+@Test
+public void Test3() {
+	Reporter.log("Test3 execute",true);
 }
 }
