cc9133a7fc8bfdf7e1b629080320b7caeab23555
roypriyasa <roypriyasa730@gmail.com>
2026-09-03 13:12:06 +0530

    successfully

--- src/test/java/testngScripts/Demo.java
+++ src/test/java/testngScripts/Demo.java
@@ -5,19 +5,31 @@ import org.testng.annotations.Test;
 
 public class Demo {
 @Test
-public void Test1() {
+public void createCoustomer() {
 
-	Reporter.log("Test1 execute",true);
-
+	Reporter.log("Coustomer Create successfully",true);
 	
 }
 
 @Test
-public void Test2() {
-	Reporter.log("Test2 execute",true);
+public void deleteCoustomer() {
+	Reporter.log("Coustomer Deleted successfully",true);
 }
 @Test
-public void Test3() {
-	Reporter.log("Test3 execute",true);
+public void addcreateCoustomer() {
+	Reporter.log("Coustomer Add successfully",true);
 }
+@Test
+public void updateCoustomer() {
+	Reporter.log("Coustomer Update successfully",true);
+}
+@Test
+public void modifyCoustomer() {
+	Reporter.log("Coustomer Modify successfully",true);
+}
+@Test
+public void vrifyCoustomer() {
+	Reporter.log("Coustomer Vrify successfully",true);
+}
+
 }
