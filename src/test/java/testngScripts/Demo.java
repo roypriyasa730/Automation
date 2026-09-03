757d6cdfb204d819cb346a15cb49a03bb8429031
roypriyasa <roypriyasa730@gmail.com>
2026-09-03 13:48:25 +0530

    add invocationCount

--- src/test/java/testngScripts/Demo.java
+++ src/test/java/testngScripts/Demo.java
@@ -4,30 +4,30 @@ import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class Demo {
-@Test
+@Test(priority=5)
 public void createCoustomer() {
 
 	Reporter.log("Coustomer Create successfully",true);
 	
 }
 
-@Test
+@Test(priority=4)
 public void deleteCoustomer() {
 	Reporter.log("Coustomer Deleted successfully",true);
 }
-@Test
+@Test(priority=3)
 public void addcreateCoustomer() {
 	Reporter.log("Coustomer Add successfully",true);
 }
-@Test
+@Test(priority=0,invocationCount=5)
 public void updateCoustomer() {
 	Reporter.log("Coustomer Update successfully",true);
 }
-@Test
+@Test(priority=1)
 public void modifyCoustomer() {
 	Reporter.log("Coustomer Modify successfully",true);
 }
-@Test
+@Test(priority=2)
 public void vrifyCoustomer() {
 	Reporter.log("Coustomer Vrify successfully",true);
 }
