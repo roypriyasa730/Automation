ff6d316de85ae55b0f47a3bd8d85e667f7b39124
roypriyasa <roypriyasa730@gmail.com>
2026-09-16 23:43:33 +0530

    Improve code formatting and consistency in MyntraDragAndDrop class

--- src/test/java/mouseaction/MyntraDragAndDrop.java
+++ src/test/java/mouseaction/MyntraDragAndDrop.java
@@ -9,18 +9,24 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class MyntraDragAndDrop {
-	
-	public static void main(String[] args) throws InterruptedException {
-		 WebDriver d= new ChromeDriver();
-	        d.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-			d.manage().window().maximize();
-			d.get("https://www.myntra.com/men-tshirts");
-            WebElement element = d.findElement(By.id("rootRail"));
-			Actions a =new Actions(d);
-			a.dragAndDropBy(element, 58, 0).perform();
-			 Thread.sleep(5000);
-		    	d.quit();
-	}
 
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver d = new ChromeDriver();
+
+        d.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        d.manage().window().maximize();
+
+        d.get("https://www.myntra.com/men-tshirts");
+
+        WebElement element = d.findElement(By.id("rootRail"));
+
+        Actions a = new Actions(d);
+
+        a.dragAndDropBy(element, 58, 0).perform();
+
+        Thread.sleep(5000);
+
+        d.quit();
+    }
 }
-
