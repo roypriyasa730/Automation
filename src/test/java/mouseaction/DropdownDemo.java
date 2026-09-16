6b746d776f41b2fabf06fb8351d2a488b189c4a8
roypriyasa <roypriyasa730@gmail.com>
2026-09-16 23:46:34 +0530

    Implement DropdownDemo functionality for selecting a month

--- src/test/java/mouseaction/DropdownDemo.java
+++ src/test/java/mouseaction/DropdownDemo.java
@@ -1,5 +1,32 @@
 package mouseaction;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
 public class DropdownDemo {
 
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver d = new ChromeDriver();
+
+        d.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        d.manage().window().maximize();
+
+        d.get("https://www.facebook.com/r.php");
+
+        WebElement month = d.findElement(By.id("month"));
+
+        Select s = new Select(month);
+
+        s.selectByVisibleText("May");
+
+        Thread.sleep(3000);
+
+        d.quit();
+    }
 }
