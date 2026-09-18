8ff9b1eee19d02f69db2ac61ec9ddbdd8ac9bb57
roypriyasa <roypriyasa730@gmail.com>
2026-09-19 00:34:49 +0530

    Remove duplicate import statement in Capturingscreenshot class

--- src/test/java/testngScripts/Capturingscreenshot.java
+++ src/test/java/testngScripts/Capturingscreenshot.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+import org.testng.annotations.Test;
 
 import com.google.common.io.Files;
 
