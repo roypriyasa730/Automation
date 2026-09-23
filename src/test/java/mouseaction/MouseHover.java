6818a10e640755c032988741a151cb1e2f77e48a
roypriyasa <roypriyasa730@gmail.com>
2026-09-23 23:59:01 +0530

    Remove obsolete test output files including CSS, JS, and XML reports to clean up the project structure.

--- src/test/java/mouseaction/MouseHover.java
+++ src/test/java/mouseaction/MouseHover.java
@@ -1,7 +1,6 @@
 package mouseaction;
 
 import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,21 +9,20 @@ import org.openqa.selenium.interactions.Actions;
 
 public class MouseHover {
 	public static void main(String[] args) throws InterruptedException {
-        WebDriver d= new ChromeDriver();
-        d.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		WebDriver d = new ChromeDriver();
+		d.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		d.manage().window().maximize();
 		d.get("https://www.vtiger.com/");
 		WebElement element = d.findElement(By.id("companyDropdown"));
-		Actions a =new Actions(d);
+		Actions a = new Actions(d);
 		a.moveToElement(element).perform();
-		
+
 		d.findElement(By.xpath("//a[text()='Contact Us']")).click();
-	    String phone=	d.findElement(By.xpath("//p[contains(text(),'9243602352')]")).getText();
-	    System.out.println(phone);
-	    Thread.sleep(4000);
-    	d.quit();
+		String phone = d.findElement(By.xpath("//p[contains(text(),'9243602352')]")).getText();
+		System.out.println(phone);
+		Thread.sleep(4000);
+		d.quit();
 
 	}
-	
 
 }
--- src/test/java/mouseaction/MyntraDragAndDrop.java
+++ src/test/java/mouseaction/MyntraDragAndDrop.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
+
 public class MyntraDragAndDrop {
 
     public static void main(String[] args) throws InterruptedException {
