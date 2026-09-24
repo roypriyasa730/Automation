84241150315a022c777334be6679da3c68b4252e
roypriyasa <roypriyasa730@gmail.com>
2026-09-24 23:48:15 +0530

    Refactor HandleingTaps for improved readability by fixing spacing and formatting

--- src/test/java/automation/HandleingTaps.java
+++ src/test/java/automation/HandleingTaps.java
@@ -10,21 +10,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HandleingTaps {
 	public static void main(String[] args) throws InterruptedException {
-		WebDriver d= new ChromeDriver();
-		
+		WebDriver d = new ChromeDriver();
+
 		d.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		d.manage().window().maximize();
 		d.get("https://demoapps.qspiders.com/ui?scenario=1");
-		
+
 		d.findElement(By.xpath("//section[text()='Popups']")).click();
-		
+
 		d.findElement(By.xpath("//section[text()='Browser Windows']")).click();
 		d.findElement(By.xpath("//a[text()='New Tab']")).click();
 		d.findElement(By.xpath("//h2[text()='Laptop']/following-sibling::button")).click();
-		Set<String> alltapid=d.getWindowHandles();
-		Iterator<String> itr=alltapid.iterator();
-		String ptab=itr.next();
-		String ctab=itr.next();
+		Set<String> alltapid = d.getWindowHandles();
+		Iterator<String> itr = alltapid.iterator();
+		String ptab = itr.next();
+		String ctab = itr.next();
 		d.switchTo().window(ctab);
 		d.findElement(By.xpath("//h1[text()='High-Performance Laptop']"));
 		System.out.println();
@@ -32,6 +32,7 @@ public class HandleingTaps {
 		d.switchTo().window(ptab);
 		Thread.sleep(3000);
 		d.quit();
+		d.close();
 
-}
+	}
 }
