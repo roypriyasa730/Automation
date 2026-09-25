b88e97b043a0aa87eea9a32bc9fa2af59ea0f7eb
roypriyasa <roypriyasa730@gmail.com>
2026-09-25 10:07:20 +0530

    Remove unnecessary empty line in HandleingTaps class

--- src/test/java/automation/HandleingTaps.java
+++ src/test/java/automation/HandleingTaps.java
@@ -3,7 +3,6 @@ package automation;
 import java.time.Duration;
 import java.util.Iterator;
 import java.util.Set;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
