9729c9097d561a56fe5221aceb7f738b30d8b946
roypriyasa <roypriyasa730@gmail.com>
2026-09-26 23:53:38 +0530

    Fix formatting in LoginPage class for consistency

--- src/test/java/mockCampus/LoginPage.java
+++ src/test/java/mockCampus/LoginPage.java
@@ -6,16 +6,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginPage {
-    public static void main(String[] args) {
-        ChromeDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://mockcampus.com/");
-        driver.findElement(By.xpath("//span[text()='Login / Register']")).click();
-        driver.findElement(By.id("student-email")).sendKeys("roypriyasa730.com");
-        driver.findElement(By.id("student-password")).sendKeys("Shine@999");
-        driver.findElement(By.xpath("//button[contains(.,'Login and continue')]")).click();
-        System.out.println("Email and password entered");
-        driver.quit();
+public static void main(String[]args) {
+    ChromeDriver driver = new ChromeDriver();
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    driver.get("https://mockcampus.com/");
+    driver.findElement(By.xpath("//span[text()='Login / Register']")).click();
+    driver.findElement(By.id("student-email")).sendKeys("roypriyasa730.com");
+    driver.findElement(By.id("student-password")).sendKeys("Shine@999");
+    driver.findElement(By.xpath("//button[contains(.,'Login and continue')]")).click();
+    System.out.println("Email and password entered");
+    driver.quit();
 
-    }
+}
 }
